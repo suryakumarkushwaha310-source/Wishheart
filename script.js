3e4095e543d4e562d9d4e37c4594e3edcebbf3e1
suryakumarkushwaha310-source <suryakumarkushwaha310@gmail.com>
2025-12-31 00:44:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 setTimeout(() => {
-  splash.classList.add("hidden");
-  home.classList.remove("hidden");
+  document.getElementById("splash").style.display = "none";
+  document.getElementById("home").classList.remove("hidden");
 }, 5000);
-
-function openCategory() {
-  home.classList.add("hidden");
-  category.classList.remove("hidden");
-}
-
-function openForm(type) {
-  category.classList.add("hidden");
-  form.classList.remove("hidden");
-  window.wishType = type;
-}
-
-function generateWish() {
-  form.classList.add("hidden");
-  wish.classList.remove("hidden");
-
-  let title =
-    wishType === "newyear"
-      ? "Happy New Year " + toName.value
-      : "Happy Birthday " + toName.value;
-
-  wishTitle.innerText = title;
-  wishMsg.innerText = message.value + " - From " + fromName.value;
-    }
