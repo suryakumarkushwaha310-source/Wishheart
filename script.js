55e5220bd1afa89d79b00c053680e032fef13214
suryakumarkushwaha310-source <suryakumarkushwaha310@gmail.com>
2025-12-31 02:08:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-setTimeout(() => {
-  document.getElementById("splash").style.display = "none";
-  document.getElementById("home").classList.remove("hidden");
-}, 5000);
+function goCategory(){
+  location.href="category.html";
+}
+
+function selectType(type){
+  localStorage.setItem("type",type);
+  location.href="form.html";
+}
+
+function saveWish(){
+  localStorage.setItem("to",to.value);
+  localStorage.setItem("from",from.value);
+  localStorage.setItem("msg",msg.value);
+  localStorage.setItem("pass",pass.value);
+  location.href="wish.html";
+}
+
+let hold;
+const heart=document.getElementById("bigHeart");
+
+if(heart){
+heart.onmousedown=()=>{
+  hold=setTimeout(()=>{
+    alert("Wish Revealed 🎉");
+  },5000);
+}
+heart.onmouseup=()=>clearTimeout(hold);
+}
