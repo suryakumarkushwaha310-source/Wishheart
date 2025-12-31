a1bd05e13ccd0d65fab013f72499daddc4bed106
suryakumarkushwaha310-source <suryakumarkushwaha310@gmail.com>
2025-12-31 09:03:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,49 @@
 let data = {};
-let holdTimer;
+let currentType = "";
+
+setTimeout(() => {
+  show("home");
+}, 5000);
 
 function show(id) {
   document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
   document.getElementById(id).classList.add("active");
 }
 
-setTimeout(() => show("home"), 5000);
-
 function openCategory() {
   show("category");
 }
 
 function openForm(type) {
-  data.type = type;
+  currentType = type;
   document.getElementById("formTitle").innerText =
     type === "newyear" ? "New Year Wish" : "Birthday Wish";
   show("form");
 }
 
 function generateWish() {
-  data.to = toName.value;
-  data.from = fromName.value;
-  data.msg = message.value;
-  data.pass = password.value;
+  data = {
+    to: toName.value,
+    from: fromName.value,
+    msg: message.value,
+    pass: password.value,
+    type: currentType
+  };
   show("passwordScreen");
 }
 
 function unlockWish() {
-  if (checkPassword.value === data.pass) {
+  if (checkPass.value === data.pass) {
+    document.getElementById("wishTitle").innerText =
+      (data.type === "newyear" ? "Happy New Year " : "Happy Birthday ") + data.to;
+    document.getElementById("wishBy").innerText = "By " + data.from;
+    document.getElementById("wishMsg").innerText = data.msg;
     show("wish");
-    startHold();
   } else {
     alert("Wrong password");
   }
 }
 
-function startHold() {
-  const heart = document.getElementById("holdHeart");
-  heart.onmousedown = () => {
-    holdTimer = setTimeout(() => {
-      openFinal();
-    }, 5000);
-  };
-  heart.onmouseup = () => clearTimeout(holdTimer);
-}
-
-function openFinal() {
-  document.getElementById("finalTitle").innerText =
-    (data.type === "newyear" ? "Happy New Year " : "Happy Birthday ") + data.to;
-  document.getElementById("finalFrom").innerText = "By " + data.from;
-  document.getElementById("finalMsg").innerText = data.msg;
-  show("final");
-}
-
-function goHome() {
-  show("home");
-}
-
 function showSaved() {
-  alert("Saved wishes feature can be added next");
+  alert("Saved links feature can be added next");
 }
