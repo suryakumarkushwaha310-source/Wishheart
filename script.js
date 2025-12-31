e68c48b579e8fffe943202e644e164ee0f4662d0
suryakumarkushwaha310-source <suryakumarkushwaha310@gmail.com>
2025-12-31 09:25:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-// Splash screen timing
+// Splash → Home after 5 sec
 setTimeout(() => {
   document.getElementById("splash").style.display = "none";
   document.getElementById("home").classList.remove("hidden");
 }, 5000);
 
-// Button action
-function goCreate() {
-  alert("Next screen coming soon 🚀");
+function showForm() {
+  document.getElementById("home").classList.add("hidden");
+  document.getElementById("form").classList.remove("hidden");
+}
+
+function generateWish() {
+  const to = document.getElementById("to").value;
+  const from = document.getElementById("from").value;
+  const msg = document.getElementById("msg").value;
+
+  document.getElementById("form").classList.add("hidden");
+  document.getElementById("wish").classList.remove("hidden");
+
+  document.getElementById("wishText").innerHTML =
+    `💌 Dear ${to}<br><br>${msg}<br><br>— ${from}`;
+}
+
+function restart() {
+  location.reload();
 }
