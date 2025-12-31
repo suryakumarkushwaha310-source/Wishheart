20542228ac3fccad032b2ff436687b56e59e2d00
suryakumarkushwaha310-source <suryakumarkushwaha310@gmail.com>
2025-12-31 08:56:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 let data = {};
+let holdTimer;
 
-setTimeout(() => {
-  splash.classList.add("hidden");
-  home.classList.remove("hidden");
-}, 5000);
-
-function openForm() {
-  home.classList.add("hidden");
-  form.classList.remove("hidden");
+function show(id) {
+  document.querySelectorAll(".screen").forEach(s => s.classList.remove("active"));
+  document.getElementById(id).classList.add("active");
 }
 
-function generate() {
-  data.to = to.value;
-  data.from = from.value;
-  data.msg = msg.value;
-  data.pass = pass.value;
-  form.classList.add("hidden");
-  lock.classList.remove("hidden");
+setTimeout(() => show("home"), 5000);
+
+function openCategory() {
+  show("category");
 }
 
-function unlock() {
-  if (check.value === data.pass) {
-    lock.classList.add("hidden");
-    wish.classList.remove("hidden");
-    title.innerText = "Happy New Year " + data.to;
-    by.innerText = "By " + data.from;
-    text.innerText = data.msg;
+function openForm(type) {
+  data.type = type;
+  document.getElementById("formTitle").innerText =
+    type === "newyear" ? "New Year Wish" : "Birthday Wish";
+  show("form");
+}
+
+function generateWish() {
+  data.to = toName.value;
+  data.from = fromName.value;
+  data.msg = message.value;
+  data.pass = password.value;
+  show("passwordScreen");
+}
+
+function unlockWish() {
+  if (checkPassword.value === data.pass) {
+    show("wish");
+    startHold();
   } else {
     alert("Wrong password");
   }
+}
+
+function startHold() {
+  const heart = document.getElementById("holdHeart");
+  heart.onmousedown = () => {
+    holdTimer = setTimeout(() => {
+      openFinal();
+    }, 5000);
+  };
+  heart.onmouseup = () => clearTimeout(holdTimer);
+}
+
+function openFinal() {
+  document.getElementById("finalTitle").innerText =
+    (data.type === "newyear" ? "Happy New Year " : "Happy Birthday ") + data.to;
+  document.getElementById("finalFrom").innerText = "By " + data.from;
+  document.getElementById("finalMsg").innerText = data.msg;
+  show("final");
+}
+
+function goHome() {
+  show("home");
+}
+
+function showSaved() {
+  alert("Saved wishes feature can be added next");
 }
